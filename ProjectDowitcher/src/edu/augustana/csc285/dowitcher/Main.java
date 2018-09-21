a8ce8fff8dcfe00ee0c73bff1f6f78b334b0d704
Nguyen Truong <nguyentruong17@augustana.edu>
2018-09-21 14:15:27 -0500

    updated version of lab 3 which has
    -a box that shows the total of frame
    -a box to enter the frame number that you want to jump to
    (this box is not completed done)

--- ProjectDowitcher/src/edu/augustana/csc285/dowitcher/Main.java
+++ ProjectDowitcher/src/edu/augustana/csc285/dowitcher/Main.java
@@ -1,17 +1,20 @@
 package edu.augustana.csc285.dowitcher;
 	
+import org.opencv.core.Core;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
-	@Override
+	//@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -22,6 +25,8 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
 		launch(args);
 	}
 }
