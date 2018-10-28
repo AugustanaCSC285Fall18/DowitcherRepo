1ff94847f513c23781ac8901a3a71ada38fb09e7
adamdonovan16 <adamdonovan16@OLINLAB204-01.augustana.edu>
2018-10-28 17:36:06 -0500

    Finished JDocs for everything in Utils Package.

--- ProjectDowitcher/src/utils/TimeUtils.java
+++ ProjectDowitcher/src/utils/TimeUtils.java
@@ -1,14 +1,24 @@
 package utils;
 
 public class TimeUtils {
+	/**
+	 * Converts the number of seconds to a minute and second format. Returns a string in MM:SS
+	 * @param seconds
+	 * @return String
+	 */
 	public static String convertSecondsToMinutes(double seconds) {
 		int secondsRounded = (int) Math.round(seconds);
 		int minuteNumber = secondsRounded % 60;
 		int remainingSeconds = secondsRounded - minuteNumber * 60;
 		return minuteNumber + ":" + remainingSeconds;
 	}
-
-	public static int convertMinutesToSeconds(String time) {
+	
+	/**
+	 * Converts a string in MM:SS format back into number of seconds. Also can take in an integer value as a string and converts it to an int if its not in MM:SS format.
+	 * @param time
+	 * @return int
+	 */
+	public static int convertMinutesToSeconds(String time){
 		int semiColonLocation = time.indexOf(":");
 		System.out.println(semiColonLocation);
 		if (semiColonLocation != -1) {
